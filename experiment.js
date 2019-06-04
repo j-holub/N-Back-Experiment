e2af780c59b9e81ca2c243e94206a09f71c800b8
Jan Holub <Jan-Holub@gmx.net>
2019-06-04 15:42:20 +0200

    code restructuring

--- experiment.js
+++ experiment.js
@@ -1,77 +1,9 @@
 // Create the timeline array that holds the experiment flow
 let timeline = []
 
-// N-Back Stimuli, 9 in Total
-const stimuli = [
-	{
-		stimulus: render_grid(1),
-		data: {
-			numeric_value: 1,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(2),
-		data: {
-			numeric_value: 2,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(3),
-		data: {
-			numeric_value: 3,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(4),
-		data: {
-			numeric_value: 4,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(5),
-		data: {
-			numeric_value: 5,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(6),
-		data: {
-			numeric_value: 6,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(7),
-		data: {
-			numeric_value: 7,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(8),
-		data: {
-			numeric_value: 8,
-			test_part: 'n-back'
-		}
-	},
-	{
-		stimulus: render_grid(9),
-		data: {
-			numeric_value: 9,
-			test_part: 'n-back'
-		}
-	}
-];
-
-
+// introduction
 const intro = introduction()
 timeline.push(intro)
-
 
 // N-Back 1-5 experiments
 // 25 in total
@@ -85,7 +17,6 @@ for(let n = 1; n<=5;n++){
 		repetitions: 5
 	});
 }
-
 
 // initialize the experiment
 jsPsych.init({
