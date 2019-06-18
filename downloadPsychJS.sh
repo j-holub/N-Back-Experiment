11b2fb14fb47649ab632586898977c5841f38416
Jan Holub <Jan-Holub@gmx.net>
2019-06-18 10:31:29 +0200

    Added ProgressBarJs to the download script

--- downloadPsychJS.sh
+++ downloadPsychJS.sh
@@ -6,3 +6,7 @@ echo "Extracting Zip Archive"
 unzip jspsych-6.0.5.zip -d PsychJS-6
 echo "Deleting Zip Archive"
 rm jspsych-6.0.5.zip
+
+echo "Downloading ProgressBarJs..."
+wget https://raw.githubusercontent.com/kimmobrunfeldt/progressbar.js/master/dist/progressbar.min.js
+mv progressbar.min.js lib/
