83a6f16a1184dba70de5bf8a048266ab7f236460
Jan Holub <Jan-Holub@gmx.net>
2019-06-18 13:28:29 +0200

    Some more paramterization

--- experiment.js
+++ experiment.js
@@ -10,17 +10,23 @@ timeline.push({
 const intro = introduction();
 timeline.push(intro);
 
+// how many trials per n step
+const trials_per_n = 4
+//  list of n steps
+const n_steps = [1,2,3,4]
+// stimuli per trial
+const stimuli = 10
 
 // create a random list of 1-4 back trials, 10 each
-const n_back_trial_sequence = jsPsych.randomization.repeat([1,2,3,4], 4);
+const n_back_trial_sequence = jsPsych.randomization.repeat(n_steps, trials_per_n);
 
 // create n-back trials according to the sequence
 n_back_trial_sequence.forEach(function(n){
 	timeline.push({
 		timeline: [
 			start_card(n),
-			n_back_experiment(n, 10),
-			end_card()
+			n_back_experiment(n, stimuli),
+			end_card(stimuli, 10000)
 		]
 	});
 });
