a3ebb89c33f9b7c6001bc47dd1a7abdd4241f9f4
Jan Holub <Jan-Holub@gmx.net>
2019-06-19 12:09:10 +0200

    Renamed the jsPsych directory and the download script

--- download_external_dependencies.sh
+++ download_external_dependencies.sh
@@ -1,9 +1,9 @@
-echo "Downloading PsychJS..."
+echo "Downloading JsPsych..."
 wget https://github.com/jspsych/jsPsych/releases/download/v6.0.5/jspsych-6.0.5.zip
 echo "Creating Directory Structure"
-mkdir -p PsychJS-6
+mkdir -p jsPsych-6
 echo "Extracting Zip Archive"
-unzip jspsych-6.0.5.zip -d PsychJS-6
+unzip jspsych-6.0.5.zip -d jsPsych-6
 echo "Deleting Zip Archive"
 rm jspsych-6.0.5.zip
 
